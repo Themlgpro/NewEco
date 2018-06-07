78ce13551f001b8842040de665795fcf9b38a6b9
GironArcila <sebastian.giron@correounivalle.edu.co>
2018-06-07 16:00:10 -0500

    inicioSesionGoogle

--- app/src/main/java/com/proyecto/appsmoviles/neweco/IndexActivity.java
+++ app/src/main/java/com/proyecto/appsmoviles/neweco/IndexActivity.java
@@ -1,13 +1,13 @@
 package com.proyecto.appsmoviles.neweco;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,19 +15,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.proyecto.appsmoviles.neweco.Database.NewEco;
+import com.google.android.gms.common.api.Status;
 
-public class IndexActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, PublicarIdea.OnFragmentInteractionListener {
-
+public class IndexActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, PublicarIdea.OnFragmentInteractionListener,GoogleApiClient.OnConnectionFailedListener{
 
 
     private NewEco conexion;
     private SQLiteDatabase bd;
     PublicarIdea pi;
+
+
+    private ImageView photoImageView;
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private TextView idTextView;
+
+    private GoogleApiClient googleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +73,93 @@ public class IndexActivity extends AppCompatActivity
         //Inicializacion y creacion;
         conexion = new NewEco(this,"NewEco",null,1);
         bd = conexion.getWritableDatabase();
+
+
+        photoImageView = (ImageView) findViewById(R.id.photoImageView);
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        emailTextView = (TextView) findViewById(R.id.emailTextView);
+        idTextView = (TextView) findViewById(R.id.idTextView);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (opr.isDone()) {
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            nameTextView.setText(account.getDisplayName());
+            emailTextView.setText(account.getEmail());
+            idTextView.setText(account.getId());
+
+
+
+
+            //Glide.with(this).load(account.getPhotoUrl()).into(photoImageView);
+
+        } else {
+            goLogInScreen();
+        }
+    }
+
+    private void goLogInScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    public void logOut(View view) {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    goLogInScreen();
+                } else {
+                    Toast.makeText(getApplicationContext(),"not close", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void revoke(View view) {
+        Auth.GoogleSignInApi.revokeAccess(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    goLogInScreen();
+                } else {
+                    Toast.makeText(getApplicationContext(),"not close", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -126,6 +229,12 @@ public class IndexActivity extends AppCompatActivity
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
--- app/src/main/java/com/proyecto/appsmoviles/neweco/LoginActivity.java
+++ app/src/main/java/com/proyecto/appsmoviles/neweco/LoginActivity.java
@@ -1,28 +1,89 @@
 package com.proyecto.appsmoviles.neweco;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements  GoogleApiClient.OnConnectionFailedListener {
 
-    private Button ingresar;
+    private SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    public  static final int SIGN_IN_CODE =777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ingresar = (Button) findViewById(R.id.logIn);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+                requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+        signInButton = (SignInButton)findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,SIGN_IN_CODE);
+            }
+        });
+
 
     }
 
-    public void logIn(View view) {
-        Intent logIn = new Intent(this, IndexActivity.class);
-        logIn.addFlags(logIn.FLAG_ACTIVITY_CLEAR_TOP | logIn.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(logIn);
+
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode==SIGN_IN_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handlesSignInResult(result);
+
+        }
+
+    }
+    private void goMainScreen() {
+        Intent intent = new Intent(this, IndexActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void handlesSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            Toast.makeText(this,"WELCOME",Toast.LENGTH_SHORT).show();
+            goMainScreen();
+        }else {
+            Toast.makeText(this,"no se pudo iniciar sesion",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
